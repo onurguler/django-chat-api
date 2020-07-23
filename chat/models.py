3340d2071a472a231d27c1b97d67dc45646e2a15
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-23 18:44:14 +0300

    Add Conversation & Message models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
+
+class Conversation(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="conversations")
+    max_members = models.IntegerField(default=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%(created_at)s" % {
+            'created_at': self.created_at
+        }
+
+
+class Message(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    to = models.ForeignKey(
+        Conversation, related_name="messages", on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%(owner)s at %(to)s on %(created_at)s" % {
+            'owner': self.owner.username,
+            'to': self.to,
+            'created_at': self.created_at,
+        }
